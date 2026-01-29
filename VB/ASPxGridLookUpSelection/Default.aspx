8b00796cbb73225c9ee7907e75ea2b652e2ab718
DevExpressExampleBot <fakeEmail>
2026-01-29 16:00:56 +0400

    Source auto update [skip ci]

--- VB/ASPxGridLookUpSelection/Default.aspx
+++ VB/ASPxGridLookUpSelection/Default.aspx
@@ -1,4 +1,4 @@
-﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="ASPxGridLookUpSelection.Default" %>
+<%@ Page Language="VB" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="ASPxGridLookUpSelection.Default" %>
 
 <%@ Register Assembly="DevExpress.Web.v16.1, Version=16.1.17.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web" TagPrefix="dx" %>
 <!DOCTYPE html>
@@ -11,14 +11,10 @@
         <div>
             <dx:ASPxMenu ID="ASPxMenu1" runat="server" Orientation="Vertical" Width="300px">
                 <Items>
-                    <dx:MenuItem NavigateUrl="~/ServerSide_SingleSelect.aspx" Text="Server Side - Single Selection">
-                    </dx:MenuItem>
-                    <dx:MenuItem NavigateUrl="~/ServerSide_MultiSelect.aspx" Text="Server Side - Multi Selection">
-                    </dx:MenuItem>
-                    <dx:MenuItem NavigateUrl="~/ClientSide_SingleSelect.aspx" Text="Client-Side - Single Selection">
-                    </dx:MenuItem>
-                    <dx:MenuItem NavigateUrl="~/ClientSide_MultiSelect.aspx" Text="Client-Side - Multi Selection">
-                    </dx:MenuItem>
+                    <dx:MenuItem NavigateUrl="~/ServerSide_SingleSelect.aspx" Text="Server Side - Single Selection" />
+                    <dx:MenuItem NavigateUrl="~/ServerSide_MultiSelect.aspx" Text="Server Side - Multi Selection" />
+                    <dx:MenuItem NavigateUrl="~/ClientSide_SingleSelect.aspx" Text="Client-Side - Single Selection" />
+                    <dx:MenuItem NavigateUrl="~/ClientSide_MultiSelect.aspx" Text="Client-Side - Multi Selection" />
                 </Items>
             </dx:ASPxMenu>
         </div>
--- VB/ASPxGridLookUpSelection/ServerSide_SingleSelect.aspx.vb
+++ VB/ASPxGridLookUpSelection/ServerSide_SingleSelect.aspx.vb
@@ -1,18 +1,18 @@
-﻿Imports System
+Imports System
 Imports DevExpress.Web
 
 Namespace ASPxGridLookUpSelection
-    Partial Public Class ServerSide_SingleSelect
-        Inherits System.Web.UI.Page
+
+    Public Partial Class ServerSide_SingleSelect
+        Inherits Web.UI.Page
 
         Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
         End Sub
 
         Protected Sub GetSelectionButton_Click(ByVal sender As Object, ByVal e As EventArgs)
             Dim grid As ASPxGridView = ASPxGridLookup1.GridView
-            Dim value As Object = grid.GetRowValues(grid.FocusedRowIndex, New String() { "ProductName" })
+            Dim value As Object = grid.GetRowValues(grid.FocusedRowIndex, New String() {"ProductName"})
             ASPxListBox1.Items.Clear()
-
             If value IsNot Nothing Then
                 ASPxListBox1.Items.Add(value.ToString())
             End If
@@ -21,17 +21,15 @@ Namespace ASPxGridLookUpSelection
         Protected Sub SetSelectionButton_Click(ByVal sender As Object, ByVal e As EventArgs)
             Dim maxUnitPrice As Integer = 0
             Dim maxUnitPriceKey As Integer = 0
-
             Dim grid As ASPxGridView = ASPxGridLookup1.GridView
             For i As Integer = grid.VisibleStartIndex To grid.VisibleRowCount - 1
-                Dim unitPrice As Integer = Convert.ToInt32(grid.GetRowValues(i, New String() { "UnitPrice" }))
+                Dim unitPrice As Integer = Convert.ToInt32(grid.GetRowValues(i, New String() {"UnitPrice"}))
                 maxUnitPrice = If(unitPrice > maxUnitPrice, unitPrice, maxUnitPrice)
                 maxUnitPriceKey = If(unitPrice = maxUnitPrice, i, maxUnitPriceKey)
-            Next i
+            Next
 
             'Prior to version 16.1.6
             'grid.Selection.SelectRow(maxUnitPriceKey);
-
             'Starting with version 16.1.6
             ASPxGridLookup1.Value = maxUnitPriceKey
         End Sub
